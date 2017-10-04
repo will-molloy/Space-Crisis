f9f35e866a651c3442dcdd1920b1df7abce18204
Unknown <yqi955@aucklanduni.ac.nz>
2017-10-04 16:33:08 +1300

    First official commit

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformFall : MonoBehaviour {
+
+	public float fallDelay = 1f;
+
+	private Rigidbody2D rb2d;
+
+	void Awake () {
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		if(other.gameObject.CompareTag("Player")) {
+			Invoke ("Fall", fallDelay);
+		}
+	}
+
+	void Fall(){
+		rb2d.isKinematic = false;
+	}
+}
---
+++ Assets/Scripts/SpawnCoins.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCoins : MonoBehaviour {
+
+	public Transform[] coinSpawns;
+	public GameObject coin;
+
+	// Use this for initialization
+	void Start () {
+	
+		Spawn ();
+	}
+
+	void Spawn() {
+		for (int i = 0; i < coinSpawns.Length; i++) {
+			int coinFlip = Random.Range (0,2);
+			if(coinFlip > 0){
+				Instantiate (coin, coinSpawns [i].position, Quaternion.identity);
+			}
+		}
+	}
+
+}
