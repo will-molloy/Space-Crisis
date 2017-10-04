38ff3fb10063c72db425e74f03b3fc952202888b
Unknown <yqi955@aucklanduni.ac.nz>
2017-10-04 17:50:51 +1300

    Reimport assets

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public int maxPlatforms = 20;
+	public GameObject platform;
+	public float horizontalMin = 6.5f; //Spwaning platform to the right of our first platform, this is the minimum distance to it
+	public float horizontalMax = 14f;
+	public float verticalMin = -6f;
+	public float verticalMax = 6f;
+
+	private Vector2 originPosition;
+
+	// Use this for initialization
+	void Start () {
+		originPosition = transform.position;
+		Spawn ();
+	}
+	
+	void Spawn(){
+		for (int i = 0; i < maxPlatforms; i++) {
+			Vector2 randomPosition = originPosition + new Vector2 (Random.Range (horizontalMin, horizontalMax), Random.Range (verticalMin, verticalMax));
+			Instantiate (platform, randomPosition, Quaternion.identity); //Quaternion.identity means no rotation
+			originPosition = randomPosition; //Next time when we run the loop we will use the last random position and run the offset there, drawing a chain of randomly spaced platforms.
+		}
+	}
+
+}
