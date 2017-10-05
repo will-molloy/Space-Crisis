e4a685f557d0209c43dbdf2adf4728280a6fd54e
Unknown <yqi955@aucklanduni.ac.nz>
2017-10-05 16:24:26 +1300

    Added dialogue box

--- Assets/Scripts/DialogHolder.cs
+++ Assets/Scripts/DialogHolder.cs
@@ -5,9 +5,10 @@ public class DialogHolder : MonoBehaviour {
 
     public string dialogue;
     private DialogueManager dMan;
+    public string[] dialogLines;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         dMan = FindObjectOfType<DialogueManager>();
 	}
 	
@@ -18,9 +19,16 @@ public class DialogHolder : MonoBehaviour {
 
     void OnTriggerStay2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
-            //.name == "Player") {
             if (Input.GetKeyUp(KeyCode.Space)) {
-                dMan.showBox(dialogue);
+
+                //dMan.showBox(this.gameObject.name , dialogue);
+
+                if (!dMan.diaglogActive) {
+                    dMan.dialogLines = dialogLines;
+                    dMan.currentLine = 0;
+                    dMan.showDialogue(this.gameObject.name);
+                }
+
             }
         }
     }
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour {
 
     public GameObject dBox;
+    public Text sText;
     public Text dText;
 
     public bool diaglogActive;
+
+    public string[] dialogLines;
+    public int currentLine;
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +21,33 @@ public class DialogueManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (diaglogActive && Input.GetKeyDown(KeyCode.Space)) {
+            //dBox.SetActive(false);
+            //diaglogActive = false;
+
+            currentLine++;
+
+        }
+
+        if (currentLine >= dialogLines.Length) {
             dBox.SetActive(false);
             diaglogActive = false;
+            currentLine = 0;
         }
-	}
+        dText.text = dialogLines[currentLine];
+    }
 
-    public void showBox(string dialogue) {
+    public void showBox(string source, string dialogue) {
         diaglogActive = true;
         dBox.SetActive(true);
+        sText.text = source;
         dText.text = dialogue;
     }
+
+    public void showDialogue(string source)
+    {
+        diaglogActive = true;
+        dBox.SetActive(true);
+        sText.text = source;
+    }
+
 }
