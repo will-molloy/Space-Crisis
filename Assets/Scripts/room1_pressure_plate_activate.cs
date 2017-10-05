7ed9e0d65b442eff8ff5daa0309e82fcf5d7ec76
wimol <willjoemolloy@gmail.com>
2017-10-05 16:24:59 +1300

    moved platforms such that users can make jump

--- Assets/Scripts/room1_pressure_plate_activate.cs
+++ Assets/Scripts/room1_pressure_plate_activate.cs
@@ -19,6 +19,13 @@ public class room1_pressure_plate_activate : MonoBehaviour
     void Update()
     {
         Debug.Log("Colliders " + colliders.Count);
+        if (colliders.Count == 0)
+        {
+            bush.setActivation(false);
+        } else
+        {
+            bush.setActivation(true);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -26,7 +33,6 @@ public class room1_pressure_plate_activate : MonoBehaviour
         if (col.gameObject.tag.Equals("Player") || col.gameObject.name.Equals("Box"))
         {
             colliders.Add(col.gameObject);
-            bush.setActivation(true);
         }
 
     }
@@ -36,10 +42,6 @@ public class room1_pressure_plate_activate : MonoBehaviour
         if (col.gameObject.tag.Equals("Player") || col.gameObject.name.Equals("Box"))
         {
             colliders.Remove(col.gameObject);
-            if (colliders.Count == 0)
-            {
-                bush.setActivation(false);
-            }
         }
     }
 
