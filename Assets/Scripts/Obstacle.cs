bbf67bdf8dc2c117ffb62981303098091b2ce5d2
Unknown <ipar569@aucklanduni.ac.nz>
2017-10-05 15:32:54 +1300

    WIP

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,10 +2,26 @@
 using System.Collections;
 
 public class Obstacle : MonoBehaviour {
-	 
-	void OnCollisionEnter(Collision col){
-		if (col.gameObject.name == "Team") {
-			Destroy (col.gameObject);
+
+	GameObject player;
+	LifeSystem life;
+	bool attacked = false;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update(){
+		if(attacked){
+			life.TakeDamage ();
+		}
+		attacked = false;
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.CompareTag ("Player")) {
+			life = other.gameObject.GetComponent<LifeSystem>();
+
 		}
 	}
 }
