063bb0cd9c049c562d3865581b77771e3a1475cb
unknown <oall605@EN-369734.UoA.auckland.ac.nz>
2017-10-05 15:22:18 +1300

    players and box now rotates plank with two pressure plates

---
+++ Assets/BushRotation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BushRotation : MonoBehaviour {
+
+    private static bool activated = false;
+
+    private float translation;
+    private int tolerance = 5;
+
+    // Use this for initialization
+    void Start () {
+
+
+}
+
+// Update is called once per frame
+void Update () {
+        translation = Time.deltaTime * 300;
+
+        GameObject pivit = GameObject.Find("bush-log-rotation-point");
+        HingeJoint2D bush = (HingeJoint2D)pivit.GetComponent(typeof(HingeJoint2D));
+
+        if (activated && transform.rotation.eulerAngles.z <= 230)
+        {
+              transform.Rotate(0, 0, translation);
+        }
+        else if (!activated && getRotation()  >= 140 && transform.rotation.eulerAngles.z <= 230+tolerance)
+        {
+             transform.Rotate(0, 0, -1 * translation);
+        }
+        Debug.Log("rotation: " + getRotation());
+    }
+
+    public void setActivation(bool active)
+    {
+        activated = active;
+    }
+
+    private int getRotation()
+    {
+        return (int)transform.rotation.eulerAngles.z;
+    }
+}
---
+++ Assets/Scripts/room1_pressure_plate_activate.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class room1_pressure_plate_activate : MonoBehaviour {
+
+    private static ArrayList colliders;
+
+	// Use this for initialization
+	void Start () {
+
+   colliders  = new ArrayList();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if (col.gameObject.tag.Equals("pressure_activator"))
+        {
+            colliders.Add(col.gameObject);
+            GameObject pivit = GameObject.Find("bush-log-rotation-point");
+            BushRotation bush = (BushRotation)pivit.GetComponent(typeof(BushRotation));
+            bush.setActivation(true);
+        }
+       
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+
+
+        if (col.gameObject.tag.Equals("pressure_activator"))
+        {
+            colliders.Remove(col.gameObject);
+            if (colliders.Count == 0)
+            {
+                GameObject pivit = GameObject.Find("bush-log-rotation-point");
+                BushRotation bush = (BushRotation)pivit.GetComponent(typeof(BushRotation));
+                bush.setActivation(false);
+            }
+        }
+    }
+
+}
