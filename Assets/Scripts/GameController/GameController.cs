54ca7f3b9a4e0ef7535c582ec56f94124d1c4e61
qhua948 <qhua948@aucklanduni.ac.nz>
2017-10-08 16:18:58 +1300

    Simple Gamecontroller

---
+++ Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController {
+
+    private static GameController instance;
+
+    private int lastScene;
+   
+    private GameController()
+    {
+        lastScene = 0;
+    }
+
+    public static GameController GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = new GameController();
+        }
+        return instance;
+    }
+
+    public void SetLastScene(int a)
+    {
+        lastScene = a;
+    }
+
+    public int GetLastScene()
+    {   
+        return lastScene;
+    }
+
+}
---
+++ Assets/Scripts/GameController/ScenePersistance.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenePersistance : MonoBehaviour {
+
+    public int ofScene;
+    private bool condition;
+    private GameObject p1, p2;
+    private Transform loc;
+    void Awake()
+    {
+        Debug.Log("THE THING AWAKENS!");
+        Debug.Log("LAST SCENCE ID " + GameController.GetInstance().GetLastScene());
+        if (GameController.GetInstance().GetLastScene() > ofScene)
+        {
+            Debug.Log("TRIHARD TRIHARD");
+            p1 = GameObject.Find("Astronaut");
+            p2 = GameObject.Find("Astronaut_2");
+            loc = GameObject.Find("PositionBack").transform;
+            p1.transform.position = loc.position;
+            p2.transform.position = loc.position;
+        }
+
+        GameController.GetInstance().SetLastScene(ofScene);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
