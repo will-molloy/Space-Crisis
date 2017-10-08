a4f1088ee53232c01f39dbe3077eb6646eb9383b
Kentrix <qhua948@aucklanduni.ac.nz>
2017-10-09 12:54:37 +1300

    added arts to doors as portals and with numbers to accurately display the count of players required to activate the portal

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -7,18 +7,29 @@ public class OpenDoor : MonoBehaviour
     public string sceneToLoad;
     private List<GameObject> colliders;
 
+    public Sprite[] sps = new Sprite[3];
+
     // Use this for initialization
     void Start()
     {
+        /*
+        sps.Add(Resources.Load("/Sprites/Portals/portal-0.png", typeof(Sprite)) as Sprite);
+        sps.Add(Resources.Load("/Sprites/Portals/portal-1.png", typeof(Sprite)) as Sprite);
+        sps.Add(Resources.Load("/Sprites/Portals/portal-2.png", typeof(Sprite)) as Sprite);
+        */
+
         colliders = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SpriteRenderer r;
+        r = GetComponent<SpriteRenderer>();
+        r.sprite = sps[2 - colliders.Count];
         if (colliders.Count == 2)
         {
-
+       
             SceneManager.LoadScene(sceneToLoad);
         }
     }
@@ -29,6 +40,8 @@ public class OpenDoor : MonoBehaviour
         {
             colliders.Add(other.gameObject);
         }
+
+       
     }
 
     void OnTriggerExit2D(Collider2D other)
