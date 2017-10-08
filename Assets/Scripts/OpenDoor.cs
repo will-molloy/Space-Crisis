de51a9a99ec82dc4759f3d5b05b105d700b6b596
Kentrix <qhua948@aucklanduni.ac.nz>
2017-10-09 12:57:16 +1300

    clean up door script

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -5,31 +5,24 @@ using UnityEngine.SceneManagement;
 public class OpenDoor : MonoBehaviour
 {
     public string sceneToLoad;
+    public Sprite[] sprites = new Sprite[3];
     private List<GameObject> colliders;
 
-    public Sprite[] sps = new Sprite[3];
 
     // Use this for initialization
     void Start()
     {
-        /*
-        sps.Add(Resources.Load("/Sprites/Portals/portal-0.png", typeof(Sprite)) as Sprite);
-        sps.Add(Resources.Load("/Sprites/Portals/portal-1.png", typeof(Sprite)) as Sprite);
-        sps.Add(Resources.Load("/Sprites/Portals/portal-2.png", typeof(Sprite)) as Sprite);
-        */
-
         colliders = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        SpriteRenderer r;
-        r = GetComponent<SpriteRenderer>();
-        r.sprite = sps[2 - colliders.Count];
+        SpriteRenderer r = GetComponent<SpriteRenderer>();
+        r.sprite = sprites[2 - colliders.Count];
+
         if (colliders.Count == 2)
         {
-       
             SceneManager.LoadScene(sceneToLoad);
         }
     }
@@ -40,8 +33,6 @@ public class OpenDoor : MonoBehaviour
         {
             colliders.Add(other.gameObject);
         }
-
-       
     }
 
     void OnTriggerExit2D(Collider2D other)
