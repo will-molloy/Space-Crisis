b51f926c0adaf5f101ca93bf9eb714d818575bc3
Unknown <ipar569@aucklanduni.ac.nz>
2017-10-09 16:15:12 +1300

    Some addition to pause game function

--- Assets/Scripts/PauseScreen.cs
+++ Assets/Scripts/PauseScreen.cs
@@ -6,10 +6,12 @@ public class PauseScreen : MonoBehaviour {
 	Timer timer;
 	public bool paused = false;
 	public GameObject pauseScreen;
+	PlatformerCharacter2D move;
 
 	// Use this for initialization
 	void Awake () {
 		timer = GameObject.Find("TimerText").GetComponent<Timer>();
+		move = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlatformerCharacter2D> ();
 	}
 	
 	// Update is called once per frame
@@ -17,10 +19,13 @@ public class PauseScreen : MonoBehaviour {
 		if (Input.GetKeyDown (KeyCode.Escape) && !paused) {
 			timer.Pause ();
 			pauseScreen.SetActive (true);
+			move.frozen = true;
+
 			paused = true;
 		} else if (Input.GetKeyDown (KeyCode.Escape) && paused) {
 			timer.Resume ();
 			pauseScreen.SetActive (false);
+			move.frozen = false;
 			paused = false;
 		}
 	}
