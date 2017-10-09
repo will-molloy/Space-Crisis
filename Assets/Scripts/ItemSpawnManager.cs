915a8efe740d76054ef2dc62226dec235d1627e8
Minha Kim <mkim907@aucklanduni.ac.nz>
2017-10-09 21:14:09 +1300

    Added Text and changed RNG

---
+++ Assets/Scripts/ItemSpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemSpawnManager : MonoBehaviour {
+
+    public Transform[] _spawns;
+    static ItemDataBaseList inventoryItemList;
+    public int[] _itemRange;
+    public List<int> _spawnedItems;
+
+    // Use this for initialization
+    void Start () {
+        _spawnedItems = new List<int>();
+        Spawn();
+
+}
+	
+	// Update is called once per frame
+	void Spawn () {
+
+        inventoryItemList = (ItemDataBaseList)Resources.Load("ItemDatabase");
+
+        for (int i = 0; i < _spawns.Length; i++)
+        {
+            int randomNumber = Random.Range(0, _itemRange.Length);
+
+            while (_spawnedItems.Contains(randomNumber)) {
+                randomNumber = Random.Range(0, _itemRange.Length);
+            }
+
+            _spawnedItems.Add(randomNumber);
+
+            if (!(inventoryItemList.itemList[_itemRange[randomNumber]].itemModel == null))
+            {
+
+                Debug.Log("Item " + _itemRange[randomNumber] + " picked!");
+                GameObject randomLootItem = (GameObject)Instantiate(inventoryItemList.itemList[_itemRange[randomNumber]].itemModel);
+                PickUpItem item = randomLootItem.AddComponent<PickUpItem>();
+                item.item = inventoryItemList.itemList[_itemRange[randomNumber]];
+
+                randomLootItem.transform.localPosition = _spawns[i].position;
+            }
+
+        }
+
+    }
+}
