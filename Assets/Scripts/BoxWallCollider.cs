bcc94dea2749637f2a34fde541096955f2e5d5d6
oliver-allen <oall605@aucklanduni.ac.nz>
2017-10-09 14:03:17 +1300

    Box can't get stuck against wall room 1

---
+++ Assets/Scripts/BoxWallCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxWallCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!col.gameObject.tag.Equals("Box"))
+        {
+            Physics2D.IgnoreCollision(col.gameObject.GetComponent<BoxCollider2D>(), GetComponent<EdgeCollider2D>());
+        }
+    }
+}
