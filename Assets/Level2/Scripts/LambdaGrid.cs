a7d2d2ccc8c41a23f547ba0f2ca527c60c39b8f7
kentrix <qhua948@aucklanduni.ac.nz>
2017-10-14 03:41:47 +1300

    New Sprites and lambda interface New changed to the scene for additional players

---
+++ Assets/Level2/Scripts/LambdaGrid.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LambdaGrid {
+
+	public const short MAX_LAMBDA_GRID_WIDTH = 8;
+	public const short MAX_LAMBDA_GRID_HEIGHT = 8;
+
+	private LambdaCube[,] lambdaActualLines {
+		get;
+		set;
+	}
+
+	public LambdaGrid() {
+        lambdaActualLines = new LambdaCube[MAX_LAMBDA_GRID_HEIGHT, MAX_LAMBDA_GRID_WIDTH];
+        for (int i = 0; i < MAX_LAMBDA_GRID_HEIGHT; i++)
+        {
+            for (int j = 0; j < MAX_LAMBDA_GRID_WIDTH; j++)
+            {
+				lambdaActualLines[i, j] = LambdaCube.NONE;
+            }
+        }
+    }
+
+	private void FallDown() {
+		bool changed = false;
+        for (int i = MAX_LAMBDA_GRID_HEIGHT - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < MAX_LAMBDA_GRID_WIDTH; j++)
+            {
+				if(lambdaActualLines[i, j] != LambdaCube.NONE
+					&& i > 0
+					&& lambdaActualLines[i-1, j] == LambdaCube.NONE) {
+					changed = true;
+					lambdaActualLines[i-1, j] = lambdaActualLines[i,j];
+					lambdaActualLines[i, j] = LambdaCube.NONE;
+				}
+            }
+        }
+		if(changed) {
+			FallDown();
+		}
+	}
+
+
+	public enum LambdaCube {
+		RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, NONE, RAINBOW, ALPHA, BETA, GAMMA, YETA
+	}
+
+	public void SimpleMap(LambdaCube from, LambdaCube to) {
+		throw new NotImplementedException();
+	}
+
+	public void Stack(LambdaCube what) {
+		throw new NotImplementedException();
+
+	}
+
+	public void StackMap(LambdaCube from, LambdaCube[] tos) {
+		throw new NotImplementedException();
+
+	}
+
+	public void Filter(LambdaCube what) {
+		throw new NotImplementedException();
+
+	}
+
+	public void FilterContains(LambdaCube what) {
+		throw new NotImplementedException();
+
+	}
+
+	public void Reverse() {
+		throw new NotImplementedException();
+
+	}
+
+	public void StackEquals() {
+		throw new NotImplementedException();
+
+	}
+
+}
--- Assets/Level2/Scripts/Level2PlayerControl.cs
+++ Assets/Level2/Scripts/Level2PlayerControl.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Level2PlayerControl : MonoBehaviour
 {
     public string horizontalKey;
