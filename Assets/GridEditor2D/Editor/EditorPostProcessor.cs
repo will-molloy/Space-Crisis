eacfee6810a072a74acc260e3ebd4ccbd3067f77
kentrix <qhua948@aucklanduni.ac.nz>
2017-10-13 21:52:14 +1300

    Inital concepts for level 2

---
+++ Assets/GridEditor2D/Editor/EditorPostProcessor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+/*This Script will autoremove all helper scripts */
+namespace GridEditor2D
+{
+    public class EditorPostProcessor
+    {
+        [PostProcessScene]
+        public static void OnPostprocessScene()
+        {
+            GameObject[] allgo = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+            for (int i = 0; i < allgo.Length; i++)
+            {
+                if (allgo[i].GetComponent<Grid>())
+                    Object.DestroyImmediate(allgo[i].GetComponent<Grid>());
+
+                if (allgo[i].GetComponent<LevelEditorLayer>())
+                    Object.DestroyImmediate(allgo[i].GetComponent<LevelEditorLayer>());
+
+                if (allgo[i].GetComponent<TileManager>())
+                    Object.DestroyImmediate(allgo[i].GetComponent<TileManager>());
+            }
+
+        }
+    }
+}
---
+++ Assets/Level2/Scripts/Level2PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Level2PlayerControl : MonoBehaviour {
+
+    public string horizontal_key;
+    public string vertical_key;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int horizontal = (int)Input.GetAxisRaw(horizontal_key);
+        int vertical = (int)Input.GetAxisRaw(vertical_key);
+	}
+}
