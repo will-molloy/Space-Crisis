a2b8eb6bbc92f1c64a825ed17a4c291ae6f76f52
Unknown <cli727@aucklanduni.ac.nz>
2017-10-20 16:16:57 +1300

    Scrollbars need to be fixed in character profile

---
+++ Assets/Scripts/CharacterContent.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class CharacterContent : MonoBehaviour
+{
+
+    public List<Text> statements;
+    private List<string> statementStrings;
+
+    // Use this for initialization
+    void Start()
+    {
+        statements = new List<Text>();
+        statementStrings = new List<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void addStatement(string statement)
+    {
+        if (!statementStrings.Contains(statement)){
+            GameObject UItext = new GameObject(statement);
+            UItext.transform.SetParent(this.gameObject.transform);
+
+            //RectTransform trans = UItext.AddComponent<RectTransform>();
+
+            Text text = UItext.AddComponent<Text>();
+            text.text = statement;
+            text.color = Color.black;
+            text.fontSize = 12;
+            text.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+            statements.Add(text);
+            statementStrings.Add(statement);
+        }
+    }
+}
