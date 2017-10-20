e5e932d879a35572913d05e50e935f1aac147531
wilmol <willjoemolloy@gmail.com>
2017-10-20 17:27:52 +1300

    seperate reset scene and level scripts, change plank to rotate with fixed update

---
+++ Assets/Scripts/GameController/ResetLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour {
+
+    public GameController.Level LevelToReset;
+
+    /**
+     * Reset all scenes in given level and put players at first scene in level
+     */
+    public void ResetScenesInLevel()
+    {
+        GameController.PlayableScene? firstSceneInLevel = null;
+
+        // Set all scenes in the level to reset when scene persistance awakes
+        foreach (GameController.PlayableScene scene in GameController.getScenesForLevel(LevelToReset))
+        {
+            if (firstSceneInLevel == null)
+                firstSceneInLevel = scene;
+            Debug.Log("Resseting: " + LevelToReset.ToString() + " ," + scene.ToString());
+            GameController.setResetSceneAttributeFor(scene, true);
+        }
+        GameController.clearScenesForLevel(LevelToReset);
+        SceneManager.LoadScene(firstSceneInLevel.ToString());
+    }
+}
--- Assets/Scripts/GameController/ResetScene.cs
+++ Assets/Scripts/GameController/ResetScene.cs
@@ -1,30 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class ResetScene : MonoBehaviour {
 
-    public GameController.Level LevelToReset;
     public GameController.PlayableScene SceneToReset;
-
-    /**
-     * Reset all scenes in given level and put players at first scene in level
-     */
-    public void ResetScenesInLevel()
-    {
-        GameController.PlayableScene? firstSceneInLevel = null;
-
-        // Set all scenes in the level to reset when scene persistance awakes
-        foreach(GameController.PlayableScene scene in GameController.getScenesForLevel(LevelToReset))
-        {
-            if (firstSceneInLevel == null)
-                firstSceneInLevel = scene;          
-            Debug.Log("Resseting: " + LevelToReset.ToString() + " ," + scene.ToString());
-            GameController.setResetSceneAttributeFor(scene, true);
-        }
-        GameController.clearScenesForLevel(LevelToReset);
-        SceneManager.LoadScene(firstSceneInLevel.ToString());
-    }
     /**
      * Reset the given scene only 
      */
