1e423f7ce1c1d090a3f645e28409b8f0f2694a20
Unknown <cli727@aucklanduni.ac.nz>
2017-10-20 19:40:17 +1300

    Statements now stored as buttons

--- Assets/Scripts/CharacterContent.cs
+++ Assets/Scripts/CharacterContent.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 public class CharacterContent : MonoBehaviour
 {
-
+    public List<Button> statementBtns;
     public List<Text> statements;
     private List<string> statementStrings;
 
@@ -27,18 +27,41 @@ public class CharacterContent : MonoBehaviour
     public void addStatement(string statement)
     {
         if (!statementStrings.Contains(statement)){
-            GameObject UItext = new GameObject(statement);
-            UItext.transform.SetParent(this.gameObject.transform);
-
-            //RectTransform trans = UItext.AddComponent<RectTransform>();
-
-            Text text = UItext.AddComponent<Text>();
-            text.text = statement;
-            text.color = Color.black;
-            text.fontSize = 14;
-            text.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
-            statements.Add(text);
             statementStrings.Add(statement);
+            /* GameObject UItext = new GameObject(statement);
+             UItext.transform.SetParent(this.gameObject.transform);
+
+             //RectTransform trans = UItext.AddComponent<RectTransform>();
+
+             Text text = UItext.AddComponent<Text>();
+             text.text = statement;
+             text.color = Color.black;
+             text.fontSize = 14;
+             text.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+             statements.Add(text);
+        */
+
+            /*create button from prefab
+            GameObject btnPrefab = (GameObject)Instantiate(Resources.Load("ButtonPrefab"));
+            Button statementBtn = btnPrefab.GetComponent<Button>();
+
+            statementBtn.tag = "StatementButton";
+            statementBtn.GetComponentInChildren<Text>().text = statement;
+            //statementBtn.transform.SetParent(FindObjectOfType<CharacterContent>().transform);*/
+
+            //-----\
+          
+            GameObject btnPrefab = (GameObject)Instantiate(Resources.Load("ButtonPrefab"),new Vector3(transform.position.x, transform.position.y), Quaternion.identity );
+            btnPrefab.transform.SetParent(this.gameObject.transform);
+            Button statementBtn = btnPrefab.GetComponent<Button>();
+
+          
+           // buttonToAdd.targetGraphic = statementBtn.targetGraphic;
+
+            statementBtn.tag = "StatementButton";
+            statementBtn.GetComponentInChildren<Text>().text = statement;
+
+            statementBtns.Add(statementBtn);
         }
     }
 }
