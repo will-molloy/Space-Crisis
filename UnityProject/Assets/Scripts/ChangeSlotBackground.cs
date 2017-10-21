53278064dd387420498b07951b00d42a77f4164c
Unknown <cli727@aucklanduni.ac.nz>
2017-10-22 12:27:58 +1300

    Have semitransparent button on top of item

---
+++ UnityProject/Assets/Scripts/ChangeSlotBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeSlotBackground : MonoBehaviour {
+
+    public Sprite redsSlot;
+    public Sprite originalSlot;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        Debug.Log("Clicked");
+        GetComponent<Image>().sprite = redsSlot;
+    }
+
+    void OnMouseExit()
+    { Debug.Log("Clicked");
+        GetComponent<Image>().sprite = originalSlot;
+    }
+
+    void OnMouseClick() {
+        GetComponent<Image>().sprite = redsSlot;
+    }
+}
