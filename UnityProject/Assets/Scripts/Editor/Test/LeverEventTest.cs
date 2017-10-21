6e61dded1bb2e0f4a7bf6a2e75c0aecf5c589650
wilmol <willjoemolloy@gmail.com>
2017-10-21 17:56:30 +1300

    begin lever tests

--- UnityProject/Assets/Scripts/Editor/Test/LeverEventTest.cs
+++ UnityProject/Assets/Scripts/Editor/Test/LeverEventTest.cs
@@ -1,22 +1,38 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
+using System.Collections.Generic;
 
-public class LeverEventTest {
+public class LeverEventTest
+{
 
-	[Test]
-	public void EditorTest()
-	{
-		//Arrange
-		var gameObject = new GameObject();
+    private GameObject lever = new GameObject();
+    private GameObject plate = new GameObject();
 
-		//Act
-		//Try to rename the GameObject
-		var newGameObjectName = "My game object";
-		gameObject.name = newGameObjectName;
+    [SetUp]
+    public void Init()
+    {
+        lever.AddComponent<Lever>();
+        lever.AddComponent<Transform>();
+        plate.AddComponent<PlateScript>();
+        plate.AddComponent<Transform>();
 
-		//Assert
-		//The object has a new name
-		Assert.AreEqual(newGameObjectName, gameObject.name);
-	}
+        plate.GetComponent<PlateScript>().translationAmount = 10;
+        plate.GetComponent<PlateScript>().translationDirection = Vector3.down;
+        plate.transform.position = new Vector3(0, 0, 0);
+
+        lever.GetComponent<Lever>().timeInFrames = 1;
+        lever.GetComponent<Lever>().thingsToControl = new List<GameObject>
+        {
+            plate,
+        };
+    }
+
+    [Test]
+    public void TestLeverSingleMovement()
+    {
+        Assert.AreEqual(new Vector3(0,0,0), plate.transform.position);
+        lever.GetComponent<Lever>().activate();
+        Assert.AreEqual(new Vector3(0,10,0), plate.transform.position);
+    }
 }
