d692135375c0c7db96296ca4c6f7b158a547f3ce
Jack Huang <huangxjack@gmail.com>
2017-10-23 17:05:23 +1300

    added persistent background music

---
+++ UnityProject/Assets/BackgroundMusicManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusicManager : MonoBehaviour {
+    private static BackgroundMusicManager instance = null;
+    public static BackgroundMusicManager Instance
+    {
+        get { return instance; }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
