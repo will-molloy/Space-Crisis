6de6116c040d03fa1c505f68d01ad6cc2f175ef9
Jack Huang <huangxjack@gmail.com>
2017-10-23 18:20:58 +1300

    upgrade to background music

--- UnityProject/Assets/BackgroundMusicManager.cs
+++ UnityProject/Assets/BackgroundMusicManager.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BackgroundMusicManager : MonoBehaviour {
     private static BackgroundMusicManager instance = null;
+
+    public List<string> Scenes = new List<string>();
     public static BackgroundMusicManager Instance
     {
         get { return instance; }
@@ -11,9 +14,13 @@ public class BackgroundMusicManager : MonoBehaviour {
     void Start () {
 	
 	}
-	
 	// Update is called once per frame
 	void Update () {
+        if (!Scenes.Contains(Application.loadedLevelName))
+        {
+            Destroy(this.gameObject);
+        }
+
         if (instance != null && instance != this)
         {
             Destroy(this.gameObject);
