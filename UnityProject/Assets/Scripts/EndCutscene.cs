11e2f535c50344e121c2201a2805644bdc6dbef1
Unknown <yqi955@aucklanduni.ac.nz>
2017-10-23 23:09:17 +1300

    Ending cutscene now exit after sometime % added skip button

---
+++ UnityProject/Assets/Scripts/EndCutscene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndCutscene : MonoBehaviour {
+
+	private float time = 60.0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (time <= 0) {
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else
+			Application.Quit();
+			#endif
+		} else if(time > 0){
+			time -= Time.deltaTime;
+		}
+	
+	}
+}
