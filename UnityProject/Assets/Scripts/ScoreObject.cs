860b012aa2c6fa9d2218b0ee51e3fac7150614d4
Minha Kim <mkim907@aucklanduni.ac.nz>
2017-10-24 02:43:25 +1300

    Added scripts but still need to intergrate them

---
+++ UnityProject/Assets/Scripts/ScoreObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreObject : MonoBehaviour {
+
+    public string _name { get; set; }
+    public int _level { get; set; }
+    public string _score { get; set; }
+    public int totalSeconds { get; set; }
+
+    public ScoreObject(string name, int lvl, string scr)
+    {
+        _name = name;
+        _level = lvl;
+        _score = scr;
+
+        string[] sstring = _score.Split(':');
+
+        int minutes = int.Parse(sstring[0]);
+        int seconds = int.Parse(sstring[1]);
+
+        totalSeconds = minutes + seconds;
+
+  
+
+
+
+    }
+
+}
---
+++ UnityProject/Assets/Scripts/TextInput.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour {
+
+    public InputField mainInputField;
+    public Text FinalScoreText;
+    public Button SubmitButton;
+
+    ScoreManager sm;
+
+    //persisting fields
+    public string TeamName;
+    public string HighScore;
+    private int levelType;
+
+    void Start()
+    {
+        sm = ScoreManager.Instance();
+        Button btn = SubmitButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+
+        HighScore = FinalScoreText.text.ToString();
+
+        if (mainInputField.text.ToString() == "") {
+            //default value
+            TeamName = "The Astronauts";
+        } else
+        {
+            TeamName = mainInputField.text.ToString();
+        }
+        Debug.Log(Application.loadedLevelName);
+        if (Application.loadedLevelName == "ExitScene")
+        {
+            levelType = 0;
+        } else
+        {
+            levelType = 1;
+        }
+
+        Debug.Log("Team: " + TeamName);
+        Debug.Log("Score: " + HighScore);
+        Debug.Log("level " + levelType);
+
+        sm.SetScore(TeamName, levelType, HighScore);
+
+
+    }
+
+
+
+}
